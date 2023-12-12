cd697a8ef430ec0d6c65c668e0ceb83a6e3ba1f0
Komron Aripov <karipov@protonmail.com>
2023-12-12 13:56:43 -0500

    proofs for a very simple language done

--- VerifiedCompiler/Compare.lean
+++ VerifiedCompiler/Compare.lean
@@ -10,19 +10,6 @@ def Processor.evalToValue (ds : List Directive) : Value :=
   Integer (Processor.eval ds)
 
 
--- lemma smaller_step
-
-lemma step
-  (prog : Expr)
-  (n : Nat)
-  (hypo : Processor.evalToValue (compile_expr (prog)) = Integer n) :
-  Processor.evalToValue (compile_expr (Sub1 prog)) = Integer (n - 1)
-  :=
-    by
-      simp [Processor.evalToValue, Processor.eval, Processor.evalToState,
-            ProcessorState.setReg, ProcessorState.opVal] at *
-      sorry
-
 theorem correctness :
   ∀ prog : Expr, Processor.evalToValue (compile_expr prog) = interpret_expr prog
   | Expr.Num n =>
@@ -33,21 +20,30 @@ theorem correctness :
   | Expr.Sub1 e =>
     by
       have ih := correctness e
-      simp [Processor.evalToValue, Processor.eval, Processor.evalToState, compile_expr] at *
-      -- simp [interpret_expr] at *
+      simp [Processor.evalToValue, Processor.eval, Processor.evalToState, interpret_expr] at *
+      rw [←ih]
+      simp
+      rw [compile_expr]
+      cases (compile_expr e) with
+      | nil => simp
+      | cons hd tl =>
+        have hfold_concat :=
+          List.foldl_concat processDirective { rax := 0 }
+            (Directive.Sub (Operand.Reg Register.Rax, Operand.Imm 1)) (hd :: tl)
+        rw [hfold_concat]
+        rfl
 
-
-
-
-
-      -- have modified_ih :
-      --   Integer (processDirective (List.foldl processDirective { rax := 0 } (compile_expr e)) (Directive.Sub (Operand.Reg Register.Rax, Operand.Imm 1))).rax
-      --   = Integer ((List.foldl processDirective { rax := 0 } (compile_expr e)).rax - 1)
-      --     :=
-
-
-
-
-
-
-  | Expr.Add1 e => by sorry
+  | Expr.Add1 e =>
+    by
+      have ih := correctness e
+      simp [Processor.evalToValue, Processor.eval, Processor.evalToState, interpret_expr] at *
+      rw [←ih]
+      simp
+      rw [compile_expr]
+      cases (compile_expr e) with
+      | nil => simp
+      | cons hd tl =>
+        have hfold_concat := List.foldl_concat processDirective { rax := 0 }
+            (Directive.Add (Operand.Reg Register.Rax, Operand.Imm 1)) (hd :: tl)
+        rw [hfold_concat]
+        rfl
